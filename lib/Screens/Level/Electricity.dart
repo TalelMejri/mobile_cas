28479fd7a66236bb63d204a698c927c9229fb5d9
TalelMejri <talelmejri8@gmail.com>
2023-12-15 00:08:28 +0100

    Electricity

---
+++ lib/Screens/Level/Electricity.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Electricity extends StatefulWidget {
+  const Electricity({super.key});
+
+  @override
+  State<Electricity> createState() => _ElectricityState();
+}
+
+class _ElectricityState extends State<Electricity> {
+
+@override
+  Widget build(BuildContext context) {
+    return  Text("Electricity");
+  }
+}
