7d34cfea59f09c79f798698f4d15c662c70aab2a
TalelMejri <talelmejri8@gmail.com>
2023-12-15 00:08:32 +0100

    Trash

---
+++ lib/Screens/Level/Trash.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Trash extends StatefulWidget {
+  const Trash({super.key});
+
+  @override
+  State<Trash> createState() => _TrashState();
+}
+
+class _TrashState extends State<Trash> {
+
+@override
+  Widget build(BuildContext context) {
+    return  Text("trash");
+  }
+}
