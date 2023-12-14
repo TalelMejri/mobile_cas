8916de2c9bb062695cd948930ff56a602ea0077b
TalelMejri <talelmejri8@gmail.com>
2023-12-14 23:41:22 +0100

    StepOne

--- lib/Screens/Steps/StepOne.dart
+++ lib/Screens/Steps/StepOne.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
 
-class stepOne extends StatelessWidget {
-  const stepOne({super.key});
+class StepOne extends StatelessWidget {
+  const StepOne({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return Text("Step One");
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        margin: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Colors.blueGrey[100],
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Success is not found in the",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            Text(
+              "value of possessed things, but",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            Text(
+              "rather in the value of things",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            Text(
+              "that possess us",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              "'Albert Einstein'",
+              style: TextStyle(
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
