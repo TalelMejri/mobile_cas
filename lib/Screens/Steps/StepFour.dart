f584ca08682750f38bb84caef509168970db6230
TalelMejri <talelmejri8@gmail.com>
2023-12-14 23:41:44 +0100

    StepFour

--- lib/Screens/Steps/StepFour.dart
+++ lib/Screens/Steps/StepFour.dart
@@ -8,8 +8,39 @@ class StepFour extends StatefulWidget {
 }
 
 class _StepFourState extends State<StepFour> {
+
+  List<String> images = [
+    "images/benif3.png",
+    "images/benif2.png",
+    "images/benif1.png",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Text("step4");
+    return Column(
+      children: [
+        SizedBox(height: 100,),
+        Text(
+          "Benifits of CAS Challenge",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        SizedBox(height: 100),
+        Container(
+          height: 300,
+          child: ListView.builder(
+            itemCount: images.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              var image = images[index];
+              return Image.asset(image, width: 300);
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
