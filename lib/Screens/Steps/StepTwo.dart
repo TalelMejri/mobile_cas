51f22e51fe3daec69834418159a2049174f4628b
TalelMejri <talelmejri8@gmail.com>
2023-12-14 23:41:30 +0100

    StepTwo

--- lib/Screens/Steps/StepTwo.dart
+++ lib/Screens/Steps/StepTwo.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
 
 class StepTwo extends StatefulWidget {
-  const StepTwo({super.key});
+  const StepTwo({Key? key});
 
   @override
   State<StepTwo> createState() => _StepTwoState();
 }
 
 class _StepTwoState extends State<StepTwo> {
+  List<String> images = [
+    "images/ieee.png",
+    "images/tsyp.jpg",
+    "images/ias.png",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Text("step2");
+    return Column(
+      children: [
+        SizedBox(height: 100,),
+        Text(
+          "CAS CHALLENGE",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        Text("TRASH BIN"),
+        SizedBox(height: 100),
+        Container(
+          height: 300,
+          child: ListView.builder(
+            itemCount: images.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              var image = images[index];
+              return Image.asset(image, width: 300);
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
