ffb068b61e2007c7e55050dd734725aac6713aca
TalelMejri <talelmejri8@gmail.com>
2023-12-14 23:41:37 +0100

    StepThree

--- lib/Screens/Steps/StepThree.dart
+++ lib/Screens/Steps/StepThree.dart
@@ -8,8 +8,41 @@ class StepThree extends StatefulWidget {
 }
 
 class _StepThreeState extends State<StepThree> {
-  @override
+
+   List<String> images = [
+    "images/1.png",
+    "images/2.png",
+    "images/3.png",
+    "images/4.png",
+  ];
+
+@override
   Widget build(BuildContext context) {
-    return Text("step3");
+    return Column(
+      children: [
+        SizedBox(height: 100,),
+        Text(
+          "Solution",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        Text("Our project is a smart bin that allows us to  produce  electricity,"),
+         Text("Biogas and natural fertilizers."),
+        SizedBox(height: 100),
+        Container(
+          height: 500, 
+          child: ListView.builder(
+            itemCount: images.length,
+            itemBuilder: (context, index) {
+              var image = images[index];
+              return Image.asset(image, width: 300);
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
